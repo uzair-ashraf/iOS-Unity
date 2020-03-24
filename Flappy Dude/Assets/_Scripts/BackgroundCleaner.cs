54f671735680fe482e5aeac29c071295cc8c3d30
Uzair Ashraf <uzinatorcl@live.com>
2020-03-23 23:35:07 -0700

    added paralax effect by changing object position with cleaner collide

---
+++ Flappy Dude/Assets/_Scripts/BackgroundCleaner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCleaner : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision);
+        if(collision.tag == "Background") {
+            float widthOfObject = ((BoxCollider2D)collision).size.x;
+            Vector3 position = collision.transform.position;
+            position.x += widthOfObject * 1.99f;
+            collision.transform.position = position;
+
+        } 
+    }
+
+}
