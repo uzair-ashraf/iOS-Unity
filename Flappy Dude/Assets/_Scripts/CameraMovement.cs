5e0d0ff4eb9c4f8dc4d5ae486846e1298d04969f
Uzair Ashraf <uzinatorcl@live.com>
2020-03-23 23:07:45 -0700

    added camera movement

---
+++ Flappy Dude/Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    float offsetX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetX = transform.position.x - playerTransform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = playerTransform.position.x + offsetX;
+
+        transform.position = position;
+    }
+}
