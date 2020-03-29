db39f729e807bf8185631c441c249b7fde4bd5af
Uzair Ashraf <uzinatorcl@live.com>
2020-03-29 16:42:50 -0700

    blocks now randomly generate

---
+++ Flappy Dude/Assets/_Scripts/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    Transform blockGenerator;
+    int blockNumber;
+    bool smallBlock;
+
+    public void SetBlockNumberAndSpawn(int _blockNumber, Transform _blockGenerator, bool _smallBlock) {
+        blockNumber = _blockNumber;
+        blockGenerator = _blockGenerator;
+        smallBlock = _smallBlock;
+
+        Vector3 pos = Vector3.zero;
+        pos.x = Camera.main.transform.position.x + 1.25f + blockNumber;
+
+        if(smallBlock) {
+            if(blockNumber % 2 == 0) {
+                pos.y = UnityEngine.Random.Range(0.40f, 1f);
+            } else {
+                pos.y = UnityEngine.Random.Range(1.7f, 2.25f);
+            }
+        } else {
+            if(blockNumber % 2 == 0) {
+                pos.y = UnityEngine.Random.Range(1.7f, 2.25f);
+            } else {
+                pos.y = UnityEngine.Random.Range(0.40f, 1f);
+            }
+        }
+
+        transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+
+    }
+
+}
---
+++ Flappy Dude/Assets/_Scripts/BlockGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour
+{
+    public List<GameObject> smallBlocks;
+    public List<GameObject> largeBlocks;
+
+    public int blocksToGenerate = 12;
+
+    int blockNumber = 1;
+
+    System.Random rand = new System.Random();
+
+    private void Awake() {
+        for(int i = 0; i < blocksToGenerate; i++) {
+            int smallBlockToGenerate = rand.Next(0, smallBlocks.Count);
+            GameObject smallBlock = GameObject.Instantiate(smallBlocks[smallBlockToGenerate]);
+            smallBlock.GetComponent<Block>().SetBlockNumberAndSpawn(blockNumber, transform, true);
+
+            int largeBlockToGenerate = rand.Next(0, largeBlocks.Count);
+            GameObject largeBlock = GameObject.Instantiate(largeBlocks[largeBlockToGenerate]);
+
+            largeBlock.GetComponent<Block>().SetBlockNumberAndSpawn(blockNumber, transform, false);
+            
+            blockNumber++;
+        }
+    }
+}
